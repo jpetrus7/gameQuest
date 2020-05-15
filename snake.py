c7f7e93ebb1d92ab5b37f5a0372fcda270954c7b
Petrus <josh.petrus21@bcp.org>
2020-05-15 12:01:48 -0700

    Update snake.py

--- snake.py
+++ snake.py
@@ -4,20 +4,21 @@
 import turtle
 import time
 import random
-
+# delay until next round
 delay = 0.1
 
-# Score
+# Score continuing throughr ounds
 score = 0
 high_score = 0
 
-# Set up the screen
+# Start up
 wn = turtle.Screen()
 wn.bgcolor("green")
 wn.setup(width=600, height=600)
-wn.tracer(0) # Turns off the screen updates
+wn.tracer(0) 
+# Turns off the screen
 
-# Snake head
+# Snake head features
 head = turtle.Turtle()
 head.speed(0)
 head.shape("square")
@@ -26,7 +27,7 @@ head.penup()
 head.goto(0,0)
 head.direction = "stop"
 
-# Snake food
+# Snake food to make snake bigger
 food = turtle.Turtle()
 food.speed(0)
 food.shape("circle")
@@ -46,7 +47,7 @@ pen.hideturtle()
 pen.goto(0, 260)
 pen.write("Score: 0  High Score: 0", align="center", font=("Times New Roman", 24, "normal"))
 
-# Functions
+# Functions to move snake
 def go_up():
     if head.direction != "down":
         head.direction = "up"
@@ -80,73 +81,73 @@ def move():
         x = head.xcor()
         head.setx(x + 20)
 
-# Keyboard bindings
+# Moving keys
 wn.listen()
 wn.onkeypress(go_up, "w")
 wn.onkeypress(go_down, "s")
 wn.onkeypress(go_left, "a")
 wn.onkeypress(go_right, "d")
 
-# Main game loop
+# Working while true
 while True:
     wn.update()
 
-    # Check for a collision with the border
+    # Collisions
     if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
         time.sleep(1)
         head.goto(0,0)
         head.direction = "stop"
 
-        # Hide the segments
+        # Hiding segments
         for segment in segments:
             segment.goto(1000, 1000)
         
-        # Clear the segments list
+        # Clear the list
         segments.clear()
 
-        # Reset the score
+        # Score shown(reset)
         score = 0
 
-        # Reset the delay
+        # Delay(reset)
         delay = 0.1
 
+        # Font and words on game
         pen.clear()
         pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Times New Rman", 24, "normal")) 
 
 
-    # Check for a collision with the food
+    # Bumbing into food
     if head.distance(food) < 20:
-        # Move the food to a random spot
+        # Food being moved to random spots in the game
         x = random.randint(-290, 290)
         y = random.randint(-290, 290)
         food.goto(x,y)
 
-        # Add a segment
+        # New segment
         new_segment = turtle.Turtle()
         new_segment.speed(0)
         new_segment.shape("square")
         new_segment.penup()
         segments.append(new_segment)
-
-        # Shorten the delay
+        
+        # Delay
         delay -= 0.001
 
-        # Increase the score
+        # Score increased for eating food
         score += 10
 
         if score > high_score:
             high_score = score
-        
+        # Pen used for font and words on game
         pen.clear()
         pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Times New Roman", 24, "normal")) 
 
-    # Move the end segments first in reverse order
+    # Range and segments 
     for index in range(len(segments)-1, 0, -1):
         x = segments[index-1].xcor()
         y = segments[index-1].ycor()
         segments[index].goto(x, y)
 
-    # Move segment 0 to where the head is
     if len(segments) > 0:
         x = head.xcor()
         y = head.ycor()
@@ -154,30 +155,30 @@ while True:
 
     move()    
 
-    # Check for head collision with the body segments
+    # Head bumbing into body
     for segment in segments:
         if segment.distance(head) < 20:
             time.sleep(1)
             head.goto(0,0)
             head.direction = "stop"
         
-            # Hide the segments
+            # Hiding the segments
             for segment in segments:
                 segment.goto(1000, 1000)
         
-            # Clear the segments list
+            # Clear
             segments.clear()
 
-            # Reset the score
+            # Score shown(reset)
             score = 0
 
-            # Reset the delay
+            # Delay(reset)
             delay = 0.1
         
-            # Update the score display
+            # Score shown with text 
             pen.clear()
             pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Times New Roman", 24, "normal"))
-
+    #Delay
     time.sleep(delay)
 
 wn.mainloop()
